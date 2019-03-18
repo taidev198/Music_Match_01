91656f1da4da5c397cff1f59d8aee922322b4460
taidev198 <traigiuxom9x@gmail.com>
2019-03-18 14:41:37 +0700

    task_8548

--- app/src/main/java/com/framgia/musixmatch/ui/main/MainActivity.java
+++ app/src/main/java/com/framgia/musixmatch/ui/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.framgia.musixmatch;
+package com.framgia.musixmatch.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.framgia.musixmatch.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/framgia/musixmatch/ui/splash/SplashActivity.java
+++ app/src/main/java/com/framgia/musixmatch/ui/splash/SplashActivity.java
@@ -1,9 +1,12 @@
-package com.framgia.musixmatch;
+package com.framgia.musixmatch.ui.splash;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+
+import com.framgia.musixmatch.R;
+import com.framgia.musixmatch.ui.main.MainActivity;
 
 /**
  * Created by superme198 on 15,March,2019
@@ -11,14 +14,18 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class SplashActivity extends AppCompatActivity {
     public static final int TIME_DELAY = 2000;
-    private Handler mHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        goToHomeScreen();
+    }
+
+    private void goToHomeScreen(){
         //start to main
-       mHandler =  new Handler();
-       mHandler.postDelayed(()->{
+        Handler mHandler =  new Handler();
+        mHandler.postDelayed(()->{
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
         },TIME_DELAY);
